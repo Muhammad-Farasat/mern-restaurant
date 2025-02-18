ba4430ee4684cae9103fdce8faf33c4bf17c0d93
Muhammad-Farasat <farasatkhan687@gmail.com>
2025-02-18 21:50:40 +0500

    Experiment-3

--- backend/index.js
+++ backend/index.js
@@ -79,8 +79,6 @@ app.get('/check', (req, res)=>{
 
 app.use(express.static(path.join(__dirname, '/dist')));
 
-
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
